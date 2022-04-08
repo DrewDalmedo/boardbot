710992520894953afc276f948e56c2916f142313
DrewDalmedo <andrewdalmedo@gmail.com>
2022-04-08 11:06:59 -0400

    Create basic code to test that dotenv and Discord.js work correctly together

--- bot/index.js
+++ bot/index.js
@@ -1,5 +1,17 @@
-// imports
+// dotenv setup
 require("dotenv").config();
 
-console.log("hello, world");
-console.log(process.env.BOT_TOKEN);
+// discord.js variables
+const { Client, Intents } = require('discord.js');
+
+// create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// once the client is ready, run the code in the anonymous function:
+client.once('ready', () => {
+    console.log('Ready!');    
+});
+
+// log the bot into discord with the token stored in .env:
+client.login(process.env.BOT_TOKEN);
+
