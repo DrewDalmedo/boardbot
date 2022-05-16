0f297782229f6641a9cd7973840c19c84ecb676d
DrewDalmedo <andrewdalmedo@gmail.com>
2022-05-16 04:25:34 -0400

    Remove unused class definition

--- bot/src/commands/user.js
+++ bot/src/commands/user.js
@@ -1,24 +1,5 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 
-class User {
-    data = () => {
-        return new SlashCommandBuilder()
-            .setName('user')
-            .setDescription('Replies with user info');
-    }
-
-    execute = interaction => {
-        interaction.reply(`Your tag: ${interaction.user.tag}\nYour ID: ${interaction.user.id}`)
-    }
-
-    toJSON() {
-        return {
-            data: this.data,
-            execute: this.execute,
-        }
-    }
-}
-
 export default {
     data: new SlashCommandBuilder()
         .setName('user')
