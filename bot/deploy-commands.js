a89689ccc84b37a475c7cbd83d11e2321a63527c
DrewDalmedo <andrewdalmedo@gmail.com>
2022-05-16 02:57:02 -0400

    Add registration of commands

---
+++ bot/deploy-commands.js
@@ -0,0 +1,16 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v9';
+import 'dotenv/config';
+
+const commands = [
+    new SlashCommandBuilder().setName('ping').setDescription('Replies with pong'),
+    new SlashCommandBuilder().setName('server').setDescription('Replies with server info'),
+    new SlashCommandBuilder().setName('user').setDescription('Replies with user info'),
+].map(command => command.toJSON());
+
+const rest = new REST({ version: '9' }).setToken(process.env.BOT_TOKEN);
+
+rest.put(Routes.applicationGuildCommands(process.env.APPID, process.env.GUILDID), { body: commands })
+    .then(() => console.log('Successfully registered application commands'))
+    .catch(console.error);
