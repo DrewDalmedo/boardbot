547591534bff40a76c39c0cdbd8806d6b99747be
DrewDalmedo <andrewdalmedo@gmail.com>
2022-05-16 02:57:11 -0400

    Add handling of commands

--- bot/index.js
+++ bot/index.js
@@ -12,6 +12,23 @@ client.once('ready', () => {
     console.log('Ready!');    
 });
 
+client.on('interactionCreate', async interaction => {
+    // no support for interations except commands yet
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === 'ping') {
+        await interaction.reply('Pong!');
+    }
+    else if (commandName === 'server') {
+        await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+    }
+    else if (commandName === 'user') {
+        await interaction.reply(`Your tag: ${interaction.user.tag}\nYour ID: ${interaction.user.id}`);
+    }
+});
+
 // log the bot into discord with the token stored in .env:
 client.login(process.env.BOT_TOKEN);
 
