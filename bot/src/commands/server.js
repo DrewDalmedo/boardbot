7e8f6d1e6935997256c8574905053d2abc15b4e7
DrewDalmedo <andrewdalmedo@gmail.com>
2022-05-16 04:22:22 -0400

    Refactor server command

---
+++ bot/src/commands/server.js
@@ -0,0 +1,10 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName('server')
+        .setDescription('Replies with server info'),
+    async execute(interaction) {
+        await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+    }
+};
